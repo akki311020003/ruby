23cb625ef572b6c6c27defa0ef8df033288cb739
akki311020003 <41486636+akki311020003@users.noreply.github.com>
2018-12-31 13:28:36 +0530

    Update python.py

--- python.py
+++ python.py
@@ -16,8 +16,12 @@ async def on_message(message):
 @client.event  
 async def on_message(message):
      if "good" in message.content:
-           await client.send_message(message.channel, 'My nature is based on your behaviour ;P')        
-
+           await client.send_message(message.channel, 'My nature is based on your behaviour ;P') 
+        
+@client.event  
+async def on_message(message):
+     if "UCA is Crazy" in message.content:
+           await client.send_message(message.channel, 'UCA is a crazy boiman')
 
         
 
