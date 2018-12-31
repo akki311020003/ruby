b0f1a871c4993c63f8e66fad2431b81a6f0f14e5
akki311020003 <41486636+akki311020003@users.noreply.github.com>
2018-12-31 13:31:48 +0530

    Update python.py

--- python.py
+++ python.py
@@ -22,6 +22,12 @@ async def on_message(message):
 async def on_message(message):
      if "UCA is Crazy" in message.content:
            await client.send_message(message.channel, 'UCA is a crazy boiman')
+        
+ @client.event  
+async def on_message(message):
+     if "r!" in message.content:
+           await client.send_message(message.channel, 'This is the Ruby Bot. Developed by Akshit, we are still in Beta Testing. Please DM Max Planck to provide suggestions in making the bot better. Mic drop.')       
+
 
         
 
